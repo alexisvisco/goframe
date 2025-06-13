8f548185eeedd2f2d8793e9abaf70140d86f05c9
Alexis Viscogliosi <Alexis.viscogliosi@outlook.fr>
2025-06-13 11:34:11 +0200

    Add introductory docs

--- docs/pages/_meta.tsx
+++ docs/pages/_meta.tsx
@@ -1,15 +1,22 @@
 export default {
   index: "Introduction",
-  another: "Another Page",
-  advanced: "Advanced (A Folder)",
+  why: "Why",
+  "getting-started": "Getting Started",
+  architecture: "Architecture",
+  database: "Database",
+  http: "HTTP",
+  worker: "Worker",
+  mails: "Mails",
+  tasks: "Tasks",
+  cli: "CLI",
   about: {
     title: "About",
-    type: "page"
+    type: "page",
   },
   contact: {
     title: "Contact ↗",
     type: "page",
     href: "https://twitter.com/shuding_",
-    newWindow: true
-  }
+    newWindow: true,
+  },
 }
