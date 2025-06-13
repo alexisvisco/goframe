de44b77afc6fa1c73057e7cfba33b73b6786384e
Alexis Viscogliosi <alexis.viscogliosi@outlook.fr>
2025-06-13 09:00:35 +0200

    refactor: update theme configuration with new project name and links

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Goframe</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/alexisvisco/goframe',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/alexisvisco/goframe/tree/main/docs',
   footer: {
-    content: 'Nextra Docs Template',
+    content: 'Goframe Documentation',
   },
 }
 
