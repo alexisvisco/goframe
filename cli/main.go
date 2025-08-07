826522112be407b7da35e8f5563c992ecd1ffcca
Alexis Viscogliosi <alexis.viscogliosi@outlook.fr>
2025-08-07 19:04:52 +0200

    Update command description for Goframe CLI

--- cli/main.go
+++ cli/main.go
@@ -17,7 +17,7 @@ func NewCmdRoot() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:           "<command> <subcommand> [flags]",
 		Short:         "Goframe CLI",
-		Long:          `GenerateHandler a new Goframe project`,
+		Long:          `Create a new Goframe project`,
 		SilenceErrors: true,
 		Annotations: map[string]string{
 			"versionInfo": "0.0.1",
