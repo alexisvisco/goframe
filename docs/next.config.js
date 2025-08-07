412737db65cc78faa6c7346e01047dd8003f72a3
Alexis Viscogliosi <alexis.viscogliosi@outlook.fr>
2025-08-07 14:28:55 +0200

    Update next.config.js

--- docs/next.config.js
+++ docs/next.config.js
@@ -11,12 +11,4 @@ export default withNextra({
   images: {
     unoptimized: true,
   },
-  eslint: {
-    // Disable ESLint during builds
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Disable TypeScript errors during builds
-    ignoreBuildErrors: true,
-  },
 });
